941b23fc38f97c9ce70578500300e38f720eae07
kumar <kumar.sendhi@gmail.com>
2018-12-15 08:24:29 +0530

    Setup ASP.NET MVC Middleware

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -17,6 +17,7 @@ namespace OdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,8 +28,9 @@ namespace OdeToFood
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseDefaultFiles();
+            
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
