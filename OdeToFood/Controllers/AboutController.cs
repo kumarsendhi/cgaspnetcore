964fdbc43fe5bc4ac758722f12da4264ff5f04e5
kumar <kumar.sendhi@gmail.com>
2018-12-15 08:44:23 +0530

    Attribute Route exclusive

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Route("about")]
+    public class AboutController
+    {
+        [Route("")]
+        public string phone()
+        {
+            return "00189898989";
+        }
+
+        [Route("address")]
+        public string address()
+        {
+            return "USA";
+        }
+    }
+}
